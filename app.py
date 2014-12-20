0a143e0f7bc86b368007d143e7e07e3aa5c9c7d9
Douglas Lenz <thedouglenz@gmail.com>
2014-12-19 23:44:53 -0500

    Custom 404 and 500 error pages

--- app.py
+++ app.py
@@ -219,5 +219,14 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
+# Custom error handlers
+@app.errorhandler(404)
+def not_found_error(error):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+	return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
