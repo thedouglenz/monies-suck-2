2585dce5a8c4f46e6cfd394f9bd5954a17c4801d
Kyle Burkholder <kyleburkholder@Kyles-MacBook-Pro.local>
2015-01-03 00:20:45 -0500

    This time I used acutal Python syntax! It helped! WHO KNEW?! The function appears to be working. Definately needs to be cleaned up but is currently working.

--- time_functions.py
+++ time_functions.py
@@ -1,18 +1,28 @@
 def month_add(month, offset):
-    """ month_add takes two paramaters: month = (1 <= month <= 12); offset(-infinity <= offset <= infinity)
+    """ month_add takes two parameters: month = (1 <= month <= 12); offset(-infinity <= offset <= infinity)
         return value: 1 <= result <= 12
 
         Take a month and an offset and add them together with appropriate wraparound behavior
         for both positive and negative offset values.
     """
-    if(month < 1 OR month > 12)
+    if month < 1 or month > 12:
         return month;#return bad value and hopefully have it be noticed.
-    current_month = month + offset;
+    if offset < 0:
+        new_offset = - (12 - offset % 12)
+    else:
+        new_offset = offset % 12
+    current_month = month + new_offset
     normalized_month = current_month % 12
 
     #ensure that the result is valid before returning value
-    if normalized_month < 1 OR normalized_month > 12:
-        result = -1
-    else
+    if normalized_month < 1 or normalized_month > 12:
+        if normalized_month == 0:
+            result = month
+        else:
+            result = -1
+    else:
         result = normalized_month
+
+    if(month + offset) % 12 == 0:
+        result = (offset % 12) + month
     return result
