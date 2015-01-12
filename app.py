5275044f7073196122a21e62f7f9786b2d7dc8e3
Douglas Lenz <thedouglenz@gmail.com>
2015-01-11 21:28:26 -0500

    FIX BUG uniqueness contraint on name of category

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ class Category(db.Model):
 	__tablename__ = 'categories'
 
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String, unique=True, nullable=False)
+	name = db.Column(db.String, nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 	user = db.relationship('User',  backref=db.backref('categories',lazy='dynamic'))
